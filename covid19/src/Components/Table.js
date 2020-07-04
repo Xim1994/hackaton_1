01ab289dd1a0daf00fdd19ca000623ca73713f57
xim <ximyb5@hotmail.com>
2020-07-04 10:10:15 +0200

    se sube el componente tabla

---
+++ covid19/src/Components/Table.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import Table from '@material-ui/core/Table';
+
+
+const CovidTable = () => {
+    const { data, setData } = useState();  
+    
+    useEffect(() => {
+
+      }, []);
+  
+    return;
+  };
+  
+  export default CovidTable;
