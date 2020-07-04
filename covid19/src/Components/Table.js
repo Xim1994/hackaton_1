e1f3741f2c563b1e0b9965075ca00f4f85572af4
MarcosIraola <iraolamarcos@gmail.com>
2020-07-04 10:59:05 +0200

    Return de la Table.js

--- covid19/src/Components/Table.js
+++ covid19/src/Components/Table.js
@@ -1,5 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import Table from '@material-ui/core/Table';
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
 
 
 const CovidTable = () => {
@@ -9,7 +15,30 @@ const CovidTable = () => {
 
       }, []);
   
-    return;
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Country</TableCell>
+                        <TableCell align="right">Cases</TableCell>
+                        <TableCell align="right">Death</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.country}
+                            </TableCell>
+                            <TableCell align="right">{row.cases}</TableCell>
+                            <TableCell align="right">{row.death}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
   };
   
   export default CovidTable;
